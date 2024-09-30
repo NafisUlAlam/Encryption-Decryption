f89ea97dbe88966d222d3bfe6f15c8d21891b68b
Nafis <ahmednafis74@gmail.com>
2024-09-30 19:22:25 +0600

    responsive output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("first-screen").addEventListener("click", (event) => {
   }
   if (event.target.tagName === "BUTTON") {
     const password = document.getElementById("password").value;
-    if (password === "cuteZahra01") {
+    if (password === "cuteZahra") {
       document.getElementById("first-screen").classList.remove("flex");
       document.getElementById("first-screen").classList.add("hidden");
       document.getElementById("second-screen").classList.remove("hidden");
@@ -30,9 +30,15 @@ document.getElementById("second-screen").addEventListener("click", (event) => {
     if (event.target.innerText === "Decrypt") {
       outputString = decrypt(inputString);
     }
+    if (outputString === "Please input atleast a single character") {
+      document.getElementById("output").innerHTML = `
+        <p class="text-sm md:text-md lg:text-2xl text-red-500 font-medium">${outputString}</p>
+        `;
+      return;
+    }
     document.getElementById("output").innerHTML = `
-    <h2 class="text-xl font-medium">Here is your ${event.target.innerText}ed string </h2>
-    <p class="text-lg text-blue-500 font-medium">${outputString}</p>
+    <h2 class="text-sm md:text-md lg:text-2xl font-medium">Here is your ${event.target.innerText}ed string </h2>
+    <p class="text-sm md:text-md lg:text-2xl text-blue-500 font-medium">${outputString}</p>
     `;
   }
 });
